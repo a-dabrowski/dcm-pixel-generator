40dee673d9584fa77225ddfb7811b99e6da1140e
Adam Dąbrowski <gipsio@gmail.com>
2018-06-18 16:52:11 +0200

    add material-ui-core

--- src/Sites.jsx
+++ src/Sites.jsx
@@ -1,7 +1,20 @@
 import React, { Component } from 'react';
 
+import Button from '@material-ui/core/Button';
+import { withStyles } from '@material-ui/core/styles';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import InputLabel from '@material-ui/core/InputLabel';
+import Input from '@material-ui/core/Input';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+
 
 class Sites extends Component {
+
     state = {
         selected: false, 
         selection: ''    
